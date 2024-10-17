decdfc19350713ec25ec48c8624245367520becd
Samiul Islam <samiulislamshad@gmail.com>
2024-10-17 16:42:07 +0600

    working animation system

---
+++ Assets/Scripts/zzz_TestScripts/AnimationLoadingFromSpriteSheets/SpriteSheetInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public class SpriteSheetInfo
+    {
+        public string name;
+        public string filePath;
+        public Sprite[] sprites;
+    }
+}

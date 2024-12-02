2313d317c4ead252694d428857b00dc948e36754
Nuruzzaman Life <nhlife2302@gmail.com>
2024-12-02 16:06:53 +0600

    Shaman enemy added not functional yet

---
+++ Assets/Scripts/Enemy/Services/ShamanEnemy.cs
@@ -0,0 +1,13 @@
+using Enemy.Models;
+using Player;
+
+namespace Enemy.Services
+{
+    public class ShamanEnemy : BaseEnemy
+    {
+        public override void Attack(PlayerController target)
+        {
+        
+        }
+    }
+}

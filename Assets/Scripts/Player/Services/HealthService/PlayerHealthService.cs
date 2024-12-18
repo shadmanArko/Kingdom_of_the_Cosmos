c5827638ce487a55e2a654f2f4a57a0d34e129d0
Samiul Islam <samiulislamshad@gmail.com>
2024-12-18 20:30:01 +0600

    health

---
+++ Assets/Scripts/Player/Services/HealthService/PlayerHealthService.cs
@@ -0,0 +1,7 @@
+﻿namespace Player.Services
+{
+    public class PlayerHealthService
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/Views/PlayerHealthView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Player.Views
+{
+    public class PlayerHealthView : MonoBehaviour
+    {
+        public float healthBar;
+        public float shieldBar;
+
+        public float maxHealthBar;
+        public float maxShieldBar;
+    }
+}
